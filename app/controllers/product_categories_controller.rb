4ad3ed8f91026fa2417db4e4b2b3d69490872077
Karl Klein <karl.klein@cashongo.co.uk>
2016-10-30 11:49:11 +0200

    refactor controllers to keep it DRY

--- app/controllers/product_categories_controller.rb
+++ app/controllers/product_categories_controller.rb
@@ -1,19 +1,18 @@
 class ProductCategoriesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
 
   def index
     @categories = ProductCategory.all
   end
 
   def show
-    @category = ProductCategory.find(params[:id])
   end
 
   def new
   end
 
   def edit
-    @category = ProductCategory.find(params[:id])
   end
 
   def create
@@ -23,7 +22,6 @@ class ProductCategoriesController < ApplicationController
   end
 
   def update
-    @category = ProductCategory.find(params[:id])
     if @category.update(params.require(:product_category).permit(:name))
       redirect_to product_categories_path
     else
@@ -32,9 +30,14 @@ class ProductCategoriesController < ApplicationController
   end
 
   def destroy
-    @category = ProductCategory.find(params[:id])
     @category.destroy
     redirect_to product_categories_path
+  end
+
+  private
+
+  def set_category
+    @category = ProductCategory.find(params[:id])
   end
 
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,31 +1,28 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def new
   end
 
   def edit
-    @product = Product.find(params[:id])
   end
 
   def create
-    @product = Product.new(params.require(:product).permit(:name, :description, :price, :photo, :product_category_ids => []))
+    @product = Product.new(product_params)
     @product.save
     redirect_to url_for products_index_url
   end
 
   def update
-    @product = Product.find(params[:id])
-
-    if @product.update(params.require(:product).permit(:name, :description, :price, :photo, :product_category_ids => []))
+    if @product.update(product_params)
       redirect_to products_path
     else
       render 'edit'
@@ -33,9 +30,18 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    @product = Product.find(params[:id])
     @product.destroy
     redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :photo, :product_category_ids => [])
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 
 end
